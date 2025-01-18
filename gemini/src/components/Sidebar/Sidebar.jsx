2524fa4c4be90bcf0dce5ee611933891600190f1
shameem <shameemkk4@gmail.com>
2025-01-18 20:39:22 +0530

    enhance Sidebar component to display recent prompts and improve Main component styling

--- gemini/src/components/Sidebar/Sidebar.jsx
+++ gemini/src/components/Sidebar/Sidebar.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { useState } from 'react'
 import './Sidebar.css'
 import { assets } from '/src/assets/assets'
+import { useContext } from 'react'
+import { Context } from '../../contex/context'
 const Sidebar = () => {
     const [extened, setExtened] = useState(false)
-
+    const { onSent, prevPrompt, setRecentPromt } = useContext(Context)
     return (
         <div className='sidebar'>
             <div className="top">
 
-                <img onClick={()=>{setExtened(prev=>!prev)}} className='menu' src={assets.menu_icon} alt="menu_icon" />
+                <img onClick={() => { setExtened(prev => !prev) }} className='menu' src={assets.menu_icon} alt="menu_icon" />
                 <div className="newchat">
                     <img src={assets.plus_icon} alt="plus_icon" />
                     {extened ? <p>New Chat</p> : null}
@@ -17,25 +19,36 @@ const Sidebar = () => {
                 {extened ?
                     <div className="recent">
                         <p className="recent-title">Recent</p>
-                        <div className="recent-entry">
-                            <img src={assets.message_icon} alt="message_icon" />
-                            <p>What is react...</p>
-                        </div>
+                        
+                            {
+                                prevPrompt.map((item, index) => {
+                                    return (
+                                        <div key={index} className="recent-entry">
+                                        
+                                            <img src={assets.message_icon} alt="message_icon" />
+                                            <p className="recent-item">
+                                                {item}
+                                            </p>
+                                        </div>
+                                    );
+                                })
+                            }
+                        
                     </div> : null}
 
             </div>
             <div className="bottom">
                 <div className="botton-item recent-entry">
                     <img src={assets.question_icon} alt="question_icon" />
-                    {extened? <p>Help</p>: null}
+                    {extened ? <p>Help</p> : null}
                 </div>
                 <div className="botton-item recent-entry">
                     <img src={assets.history_icon} alt="history_icon" />
-                    {extened?<p>Activity</p>:null}
+                    {extened ? <p>Activity</p> : null}
                 </div>
                 <div className="botton-item recent-entry">
                     <img src={assets.setting_icon} alt="setting_icon" />
-                    {extened?<p>Settings</p>:null}
+                    {extened ? <p>Settings</p> : null}
                 </div>
 
             </div>
