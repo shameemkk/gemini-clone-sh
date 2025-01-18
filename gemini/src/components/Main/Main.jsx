59ad637f4e94a3d484dbf140f085c2d8b743453c
shameem <shameemkk4@gmail.com>
2025-01-18 19:35:49 +0530

    add Google Generative AI integration and enhance Main component functionality

--- gemini/src/components/Main/Main.jsx
+++ gemini/src/components/Main/Main.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React ,{ useContext } from 'react'
 import './Main.css'
 import {assets} from '../../assets/assets'
+import { Context } from '../../contex/context'
 const Main = () => {
+
+const {onSent,recentPrompt,showResult,loading,resultData,input,setInput}=useContext(Context)
   return (
     <div className='main'>
         <div className="nav">
@@ -9,7 +12,9 @@ const Main = () => {
             <img src={assets.user_icon} alt="profile" />
         </div>
         <div className="main-container">
-            <div className="greet">
+            {!showResult
+            ?<>
+                <div className="greet">
                 <p><span>Hellon, Dev.</span></p>
                 <p>How can I help you today?</p>
             </div>
@@ -31,13 +36,31 @@ const Main = () => {
                     <img src={assets.bulb_icon} alt="" />
                 </div>
             </div>
+            </> : <div className='result'>
+                <div className="result-title">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" />
+                    {loading?<div className='loader'>
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>
+                    :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                }
+                </div>
+            </div>
+            }
+            
             <div className="main-bottom">
                 <div className="search-box">
-                    <input type='text' placeholder='Enter a promt here'></input>
+                    <input onChange={(e)=>{setInput(e.target.value)}} value={input} type='text' placeholder='Enter a promt here'></input>
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img src={assets.send_icon} alt="" />
+                        <img onClick={()=>{onSent()}} src={assets.send_icon} alt="" />
                     </div>
                 </div>
                 <p className="bottom-info">
---
+++ gemini/src/contex/context.jsx
@@ -0,0 +1,49 @@
+import { createContext, useState } from 'react';
+import run from '../config/gemini';
+
+export const Context = createContext(); 
+
+const ContextProvider = (props) => {
+    const [input, setInput] = useState('');
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompt, setPrevPrompt] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
+
+    const onSent = async (prompt) => {
+        setResultData("");
+        setLoading(true);
+        setShowResult(true);
+        setRecentPrompt(input);
+        const response = await run(input);
+        setResultData(response);
+        setLoading(false);
+        setInput("");
+    
+    };
+
+    const contextValue = {
+        input,
+        setInput,
+        recentPrompt,
+        setRecentPrompt,
+        prevPrompt,
+        setPrevPrompt,
+        showResult,
+        setShowResult,
+        loading,
+        setLoading,
+        resultData,
+        setResultData,
+        onSent
+    };
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    );
+};
+
+export default ContextProvider
